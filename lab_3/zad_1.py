49b5dce97ffa19a162d3d3409c87375194bf53f4
matadamiec <93861718+matadamiec@users.noreply.github.com>
2021-11-23 22:15:22 +0100

    Poprawa struktury katalogu

---
+++ lab_3/zad_1.py
@@ -0,0 +1,17 @@
+"""
+Zad 1
+Stworzyć funkcję, która przyjmuje 2 argumenty (typu string ) name i surname , a
+następnie zwróci stringa zgodnie ze wzorem Cześć {name} {surname}! Należy
+uruchomić funkcję, wynik wykonania funkcji zapisać do zmiennej, a następnie go
+wyświetlić ( print)
+"""
+
+
+def name_and_surname(name: str, surname: str) -> str:
+    full_name = str("Cześć " + name + " " + surname + "!")
+    return full_name
+
+
+result = name_and_surname("Mateusz", "Adamiec")
+print("name_and_surname result:")
+print(result)
---
+++ lab_3/zad_4.py
@@ -0,0 +1,18 @@
+"""
+Zad 4
+Stworzyć funkcję, która przyjmie 3 argumenty typu int i sprawdzi czy suma dwóch
+pierwszych liczb jest większa lub równa trzeciej, a następnie zwróci tę informację
+jako typ logiczny bool
+"""
+
+
+def check_sum(number_one:  int, number_two: int, number_three: int) -> bool:
+    sum_to_check = number_one + number_two
+    if sum_to_check >= number_three:
+        return True
+    else:
+        return False
+
+
+print("check_sum result:")
+print(check_sum(10, 5, 15))
